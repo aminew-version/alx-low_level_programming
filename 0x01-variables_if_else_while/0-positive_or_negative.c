106eb5611f5f312e93c37f4e5ee0f875fc1e4edd
ermiyas <zelekealmi93@gmail.com>
2023-02-18 17:53:18 +0300

    the prgram assign different value of intiger to n every time it takes and display with their sign.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("please enter the number:\n ");
+	scanf("the number entered:", &n);
+	if (n == 0)
+	{
+		printf("%d", "n is zero\n");
+	}
+	if (n > 0)
+	{
+		printf("%d", "n is positive\n");
+	}
+	if (n < 0)
+	{
+		printf("%d", "n is negative\n");
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+0x01. C - Variables, if, else, while, this project is about the c program task on c- variables,if,else and while
