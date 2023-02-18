31970cbf86264dcae064ecc458b5270566917660
ermiyas <zelekealmi93@gmail.com>
2023-02-18 23:41:23 +0300

    lower case letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  * main - 2. I sometimes suffer from insomnia.
  * Return: 0
  */
 int main(void)
 {
-	int ch;
+	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
-		ch++;
 	}
+	putchar('\n');
 	return (0);
 }
