99d4c3fb993a2e8f455979ea4522dc5ff2a24f75
ermiyas <zelekealmi93@gmail.com>
2023-02-18 23:05:27 +0300

    lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - 2. I sometimes suffer from insomnia.
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 'a';
+
+	if (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
