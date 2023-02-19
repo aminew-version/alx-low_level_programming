d5b725a83942031bdc0c02b7d58393902ec139a7
ermiyas <zelekealmi93@gmail.com>
2023-02-19 21:55:00 +0300

    printing the small letters in reversing order using putschar function

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Smile in the mirror
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	n = 'z';
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	return (0);
+}
