fe2d66f0682e6ea3e4d515e85eaa133adc3e8c5f
ermiyas <zelekealmi93@gmail.com>
2023-02-20 23:05:58 +0300

    combination of two single digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - combinations of two single-digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int b;
+
+	for (c = 0; c <= 9; c++)
+	{
+	for (b = 0; b <= 9; b++)
+	{
+		if (c < b)
+		{
+			putchar((c % 10) + '0');
+			putchar((b % 10) + '0');
+			if (!(c == 9 && b == 9))
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	}
+	putchar('\n');
+}
