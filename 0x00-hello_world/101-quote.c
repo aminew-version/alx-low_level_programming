23c48abc0586502e73d9908db7bd26562f8d1c86
ermiyas <zelekealmi93@gmail.com>
2023-02-20 14:46:21 +0300

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main-UNIX is basically a simple operating system, bu *t you have to be a genius to understand the simplicit * y
+ * Return: 0
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
