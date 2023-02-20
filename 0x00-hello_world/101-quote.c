613f663383a228fe359dec4ffe576d43f097dfc4
ermiyas <zelekealmi93@gmail.com>
2023-02-20 15:06:24 +0300

    error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
- * main-UNIX is basically a simple operating system, bu *t you have to be a genius to understand the simplicit * y
+ * main- Return 1
  * Return: 0
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
