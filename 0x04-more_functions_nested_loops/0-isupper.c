210062327861a5d8741b5d504a573e32c81170f6
ermiyas <zelekealmi93@gmail.com>
2023-02-23 22:14:42 +0300

    Return 1 if uppercase return o if not

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _putchar - prints characters
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		_putchar('49');
+	}
+	else
+	{
+		_putchar('48');
+	}
+	return (0);
+
+}
