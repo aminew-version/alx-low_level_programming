0ada65f4d20bc056af333108c4bfd5640ba3961b
ermiyas <zelekealmi93@gmail.com>
2023-02-24 13:03:10 +0300

    determine wheather or not digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digits
+ * @c: value of the input
+ * Return: 1 if true, 0 if not digit
+ */
+int _isdigit(int c)
+{
+	if (c <= 57 && c >= 48)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
