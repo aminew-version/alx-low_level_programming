f8ac1525778594f54b30b25393562ca95b3d40af
ermiyas <zelekealmi93@gmail.com>
2023-02-24 14:10:40 +0300

    List numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - function which print 0-9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char c;
+
+	c = 48;
+
+	while (c <= 57)
+	{
+		_putchar(c);
+		c++;
+	}
+		_putchar('$');
+}
