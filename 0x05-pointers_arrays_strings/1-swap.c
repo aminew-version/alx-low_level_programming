c372e04b27d2cb6924c5b83c7adc6ea8247fcdfc
ermiyas <zelekealmi93@gmail.com>
2023-02-27 15:16:35 +0300

    swaping numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - interchang the values of a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return:
+ */
+void swap_int(int *a, int *b)
+{
+	*a = temp;
+	*a = *b;
+	temp = *b;
+}
