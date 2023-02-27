b867f0262995bb5f99a9452d179ce2050b79a91c
Amanuel <111165411+aminew-version@users.noreply.github.com>
2023-02-27 19:03:32 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,6 +8,17 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	strcpy(dest, src);
-	return (0);
+	int cl, i;
+
+	cl = 0;
+	while (src[cl] != '\0')
+	{
+		cl++;
+	}
+	for (i = 0; i < cl; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
