7dc85fa597d0c2d5ea4f338ee825191621fdba43
ermiyas <zelekealmi93@gmail.com>
2023-03-01 21:36:31 +0300

    concatenate two strings up to n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x = 0;
+
+	int v = 0;
+
+	int n = 0;
+
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	while (src[v] != '\0' && src[v] <= n)
+	{
+		dest[x] = src[v];
+		x++;
+		v++;
+	}
+	dest[x] = ('\0');
+	return (dest);
+}
