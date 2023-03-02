e0832316af9f3cd3059496a922686cc73b5b9ba6
Amanuel <111165411+aminew-version@users.noreply.github.com>
2023-03-03 01:08:37 +0300

    print intigers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,5 +6,29 @@
  */
 void print_number(int n)
 {
+	unsigned int q, r, count;
 
+	if (n < 0)
+	{
+		_putchar(45);
+		q = n * -1;
+	}
+	else
+	{
+		q = n;
+	}
+
+	r = q;
+	count = 1;
+
+	while (r > 9)
+	{
+		r /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((q / count) % 10) + 48);
+	}
 }
