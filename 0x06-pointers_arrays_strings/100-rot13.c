0e8f2576792324f4343cf5f63b1a9e1bcb34cafc
ermiyas <zelekealmi93@gmail.com>
2023-03-03 00:00:04 +0300

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * rot13 - encode string using root13
+ * @st: string
+ *
+ * Return: char
+ */
+char *rot13(char *)
+{
+	int i;
+
+	for (i = 0; st[i] != '\0'; i++)
+	{
+
+	}
+}
