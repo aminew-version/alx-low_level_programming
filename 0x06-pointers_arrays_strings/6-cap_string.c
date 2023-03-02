ec81ec0d39d24c3be85dbe5d756786557f79e552
Amanuel <111165411+aminew-version@users.noreply.github.com>
2023-03-02 21:30:51 +0300

    capitalize eachword

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,19 +3,29 @@
  * cap_string - captilize all
  * Return: char
  */
-char *cap_string(char *)
+char *cap_string(char *st)
 {
-	int i;
+	int x, y;
 
-	char a[1024];
+	char separetor[20] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
-	for (i = 0; a[i] != '\0'; i++)
+	for (x = 0; st[x] != '\0'; x++)
 	{
-		if (a[i] = 32 && a[i] = 10 && a[i] = 44 && a[i] = 59 && a[i] = 46 && a[i] = 33 && a[i] = 63 && a[i] = 34 && a[i] = 40 && a[i] = 41 && a[i] = 123 && a[i] = 125)
+		if (x == 0 && st[x] >= 'a' && st[x] <= 'z')
+			st[x] -= 32;
+
+		for (y = 0; y < 13; y++)
 		{
-			if(a[i + 1] >= 'a' && a[i + 1] <= 'z')
-				a[i + 1] = a[i + 1] - 32;
+			if (st[x] == separetor[y])
+			{
+				if (st[x + 1] >= 'a' && st[x + 1] <= 'z')
+				{
+					st[x + 1] -= 32;
+				}
+			}
 		}
-		return (a[i]);
 	}
+
+	return (st);
 }
