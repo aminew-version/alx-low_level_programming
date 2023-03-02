fe9c5e490bcf13b4da126c4a1be0007e9be8cebf
ermiyas <zelekealmi93@gmail.com>
2023-03-02 21:56:28 +0300

    encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * leet - encodes string in to 1337
+ * @s: aray of string
+ * Return: 0
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char special[20] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if 
+	}	
+}
