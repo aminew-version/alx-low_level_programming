5f57635f9176a6782d96eb6458bb1fa6f947dfed
ermiyas <zelekealmi93@gmail.com>
2023-03-02 12:37:50 +0300

    coparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-    for (int i = 0;  ; i++)
-    {
-        if (s1[i] == '\0' && s2[i] == '\0')
-        {
-            return (0);
-        }
-    int a1 = (int) s1[i];
-    int a2 = (int) s2[i];
-    if (a1 > a2)
-        return (15);
-    if (a2 > a1)
-        return (-15);
-    }
+	for (int i = 0;  ; i++)
+	{
+	if (s1[i] == '\0' && s2[i] == '\0')
+	{
+		return (0);
+	}
+	int a1 = (int) s1[i];
+
+	int a2 = (int) s2[i];
+
+	if (a1 > a2)
+		return (15);
+	if (a2 > a1)
+		return (-15);
+	}
 }
