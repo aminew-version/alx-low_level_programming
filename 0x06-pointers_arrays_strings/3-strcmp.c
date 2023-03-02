cc9362c4d8b996e7b1ead0af2d8bee9f98293b69
Amanuel <111165411+aminew-version@users.noreply.github.com>
2023-03-02 13:04:40 +0300

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	for (int i = 0;  ; i++)
+	int i;
+	
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-	if (s1[i] == '\0' && s2[i] == '\0')
-	{
-		return (0);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-	int a1 = (int) s1[i];
-
-	int a2 = (int) s2[i];
-
-	if (a1 > a2)
-		return (15);
-	if (a2 > a1)
-		return (-15);
-	}
+	return (0);
 }
