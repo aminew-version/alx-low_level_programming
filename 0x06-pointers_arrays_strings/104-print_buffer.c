a70b36874b929dc0bf41fc5204efadba6847717b
Amanuel <111165411+aminew-version@users.noreply.github.com>
2023-03-03 20:53:40 +0300

    print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,5 +8,21 @@
  */
 void print_buffer(char *b, int size)
 {
+	int y;
 
+	for (y = 0; y <= (size - 1) / 10 && size; y++)
+	{
+		printf("%08x: ", y * 10);
+		if (y < size / 10)
+		{
+			print_line(b, 9, y);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, y);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
 }
