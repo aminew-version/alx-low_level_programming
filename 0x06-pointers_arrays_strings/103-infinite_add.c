35040081d10a7d19765acdc677c11a13c0d3f24d
Amanuel <111165411+aminew-version@users.noreply.github.com>
2023-03-03 20:39:33 +0300

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,43 +8,38 @@
  *
  * Return: 0
  */
-void print_buffer(char *b, int size)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int o, j, i;
-
-	o = 0;
-
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-	while (o < size)
-	{
-		j = size - o < 10 ? size - o : 10;
-		printf("%08x: ", o);
-		for (i = 0; i < 10; i++)
-		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
-			else
-				printf("  ");
-			if (i % 2)
-			{
-				printf(" ");
-			}
-		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + o + i);
-
-			if (c < 32 || c > 132)
-			{
-				c = '.';
-			}
-			printf("%c", c);
-		}
-		printf("\n");
-		o += 10;
-	}
+int x, y, k, l, m, n;
+for (x = 0; n1[x]; x++)
+;
+for (y = 0; n2[y]; y++)
+;
+if (x > size_r || y > size_r)
+return (0);
+m = 0;
+for (x -= 1, y -= 1, k = 0; k < size_r - 1; x--, y--, k++)
+{
+n = m;
+if (x >= 0)
+n += n1[x] - '0';
+if (y >= 0)
+n += n2[j] - '0';
+if (x < 0 && y < 0 && n == 0)
+{
+break;
+}
+m = n / 10;
+r[k] = n % 10 + '0';
+}
+r[k] = '\0';
+if (x >= 0 || y >= 0 || m)
+return (0);
+for (k -= 1, l = 0; l < k; k--, l++)
+{
+m = r[k];
+r[k] = r[l];
+r[l] = m;
+}
+return (r);
 }
